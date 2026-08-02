c62c023cc8aa241c719af23c037e691f790c4c73
varuncareergize <varun.124150@gmail.com>
2026-08-02 10:48:56 +0530

    registration_page_added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Home from "./pages/Home";
 import AboutPage from "./pages/AboutPage";
 import ContactUs from "./components/ContactUs";
 import Login from "./components/Login";
+import Registration from "./components/Registration";
 import Dashboard from "./components/Dashboard";
 import ScrollToHash from "./components/ScrollToHash";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 export default function App() {
   const location = useLocation();
-  const hideHeader = location.pathname === "/login";
+  const hideHeader = location.pathname === "/login" || location.pathname === "/signup";
 
   return (
     <div className="min-h-screen bg-white text-gray-900 antialiased">
@@ -23,6 +24,7 @@ export default function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Registration />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
 
